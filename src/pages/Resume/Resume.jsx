0762d468cbf768d0dec4d8753635106b4dd073e1
Migdelina Buelna <125370406+migdelinabuelna@users.noreply.github.com>
2023-06-11 16:22:45 -0400

    portfolio resume e-mail update

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -9,7 +9,7 @@ export default function Resume() {
                 <a href="https://docs.google.com/document/d/1VOkuiwfgCKMy-CdN_JQnA6oMaXM37PmDxn9Znd75yYA/edit?usp=sharing"> <Icon name="google" color="yellow"/>RESUME</a>
             </div>
             <Divider />
-            <Image src="https://i.imgur.com/wwCnjQO.png"></Image>
+            <Image src="https://i.imgur.com/zsWx7HQ.png"></Image>
         </Container>
         </>
     )
